2020877d9581443952d155b812682d02a4fa697a
최수현 <86158560+choi-su-hyun@users.noreply.github.com>
2024-11-22 19:40:19 +0900

    feat :: 날짜, 시간 date picker input 컴포넌트 제작

--- src/pages/register-invite/index.tsx
+++ src/pages/register-invite/index.tsx
@@ -1,8 +1,11 @@
 import Button from "@/components/common/Button/Button";
+import DateInput from "@/components/common/Input/DateInput";
 import TextInput from "@/components/common/Input/TextInput";
 import Seo from "@/components/Seo";
+import dayjs from "dayjs";
 import { NextPage } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { FieldErrors, useForm } from "react-hook-form";
 
 interface Props {
@@ -10,15 +13,32 @@ interface Props {
 }
 interface FormInput {
   title: string;
+  date: string;
+  time: string;
+  postCode: string;
+  address: string;
+  addressDetail: string;
 }
+
+// ============================= 날짜 START =============================
+const today = dayjs();
+// ============================= 날짜 END =============================
 
 const RegisterInvite: NextPage<Props> = ({}) => {
   const {
     handleSubmit,
     register,
     clearErrors,
+    watch,
     formState: { errors },
-  } = useForm<FormInput>();
+  } = useForm<FormInput>({
+    defaultValues: {
+      date: today.add(1, "day").format("YY-MM-DD"), // 초기값 설정
+      time: today.format("HH:mm"), // 초기값 설정
+    },
+  });
+
+  const router = useRouter();
 
   const onValid = (data: FormInput) => {
     // TODO : API 보낼 때 에러 텍스트 관련해서 유의하자
@@ -28,6 +48,9 @@ const RegisterInvite: NextPage<Props> = ({}) => {
   const onInValid = (errors: FieldErrors) => {
     console.log(`errors : `, errors);
   };
+
+  const dateValue = watch("date");
+  const timeValue = watch("time");
 
   return (
     <>
@@ -50,6 +73,81 @@ const RegisterInvite: NextPage<Props> = ({}) => {
                 placeholder="제목을 입력해주세요"
                 errorText={errors.title?.message}
               />
+              <DateInput
+                label="약속날짜"
+                dateRegister={register("date", {
+                  required: { value: true, message: "제목을 입력해주세요." },
+                  maxLength: 8,
+                })}
+                dateValue={dateValue}
+                dateName="date"
+                datePlaceholder="0000-00-00"
+                dateErrorText={errors.date?.message}
+                timeRegister={register("time", {
+                  required: { value: true, message: "제목을 입력해주세요." },
+                  maxLength: 8,
+                })}
+                timeValue={timeValue}
+                timeName="time"
+                timePlaceholder="00:00"
+                timeErrorText={errors.time?.message}
+              />
+
+              <div className="address-wrap">
+                {/* 우편번호 */}
+                <div className="address-input-wrap">
+                  <TextInput
+                    register={register("postCode", {
+                      required: {
+                        value: true,
+                        message: "주소 검색을 통해 주소를 입력해주세요",
+                      },
+                    })}
+                    type="text"
+                    label="주소"
+                    name="postCode"
+                    placeholder="우편번호"
+                    disabled
+                    errorText={errors.postCode?.message}
+                  />
+                  <Button
+                    type="button"
+                    variant="secondary-btn"
+                    size="size-x-small"
+                    onClick={() => router.push("?modal=true")}
+                  >
+                    주소 검색
+                  </Button>
+                </div>
+
+                {/* 주소 */}
+                <TextInput
+                  register={register("address", {
+                    required: {
+                      value: true,
+                      message: "주소 검색을 통해 주소를 입력해주세요",
+                    },
+                  })}
+                  type="text"
+                  name="address"
+                  placeholder="주소"
+                  errorText={errors.address?.message}
+                />
+
+                {/* 상세 주소 */}
+                <TextInput
+                  register={register("addressDetail", {
+                    required: {
+                      value: true,
+                      message: "상세 주소를 입력해주세요",
+                    },
+                  })}
+                  type="text"
+                  name="addressDetail"
+                  placeholder="상세 주소"
+                  errorText={errors.addressDetail?.message}
+                />
+              </div>
             </div>
             <div className="btn-wrap btn-wrap--row">
               <Button size="size-small" type="button">
